4c96c7ff75fde6412e9025f18f7a985ed1d464fd
jonny0702 <jonathanvergararuiz@gmail.com>
2023-04-07 00:45:36 -0500

    :sparkles: FEATURE: build the voxelModel

--- src/Components/VoxelModel.tsx
+++ src/Components/VoxelModel.tsx
@@ -1,7 +1,29 @@
-export const VoxelModel = () =>{
+import { Canvas } from "@react-three/fiber";
+import { Model } from "./Scene";
+import "../styles/VoxelModel.sass";
+import { OrbitControls } from "@react-three/drei";
+import { Suspense } from "react";
+
+const Controller = () => {
+  return <OrbitControls enableDamping  />;
+};
+
+export const VoxelModel = () => {
   return (
     <>
-    <div>VOXEL</div>
+      <div className="Model_container">
+        <Suspense fallback={null}>
+          <Canvas>
+            <perspectiveCamera castShadow/>
+            <ambientLight  intensity={.4}/>
+            <pointLight position={[5, 1, 1]} />
+            <mesh>
+              <Model position={[0, -10, -20]} />
+            </mesh>
+            <Controller />
+          </Canvas>
+        </Suspense>
+      </div>
     </>
   );
-}
+};
