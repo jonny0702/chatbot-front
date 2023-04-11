714ac9865c9a72bf3b909b42e08a8b5a6ca129c9
jonny0702 <jonathanvergararuiz@gmail.com>
2023-04-11 10:19:50 -0500

    :lipstick: ADD: Adding Loading State for UI and UX experience

---
+++ src/Components/TypingLoading.tsx
@@ -0,0 +1,11 @@
+import "../styles/TypingLoading.sass"
+
+export const TypingLoading = () => {
+  return (
+    <div className="loader">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
